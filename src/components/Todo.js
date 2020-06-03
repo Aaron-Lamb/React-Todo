78d80376d34bbbbbf72ee8212cede8aed52e2ae0
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-06-03 14:48:08 -0700

    Added a className and onClick to the div in the Todo component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Todo = props => {
     return (
-        <div>
+        <div
+            className={`item.${props.item.completed ? 'completed' : ''}`}
+            onClick={() => props.toggleItem(props.item.id)}
+        >
             <h4>{props.item.name}</h4>
         </div>
     )
